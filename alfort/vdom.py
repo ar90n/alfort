f7f10ca96a87f092873b26453b8f2e32db2932ab
Masahiro Wada <argon.argon.argon@gmail.com>
2022-04-15 06:13:25 +0000

    feat: Add app

--- alfort/vdom.py
+++ alfort/vdom.py
@@ -1,20 +1,25 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
 from itertools import zip_longest
-from typing import Any, Generic, Protocol, TypeAlias, TypeVar
+from typing import Any, Callable, Generic, Optional, Protocol, TypeAlias, TypeVar, Union
 
-
-@dataclass(slots=True, frozen=True)
-class PatchReplace:
-    tag: str
-    props: dict[str, Any]
-    children: list[Any]
+T = TypeVar("T")
 
 
 @dataclass(slots=True, frozen=True)
 class PatchProps:
-    props: dict[str, Any]
+    remove_keys: list[str]
+    add_props: dict[str, Any]
+
+
+@dataclass(slots=True, frozen=True)
+class PatchInsertChild:
+    child: Any
+    reference: Any
+
+
+@dataclass(slots=True, frozen=True)
+class PatchRemoveChild:
+    child: Any
 
 
 @dataclass(slots=True, frozen=True)
@@ -22,15 +27,10 @@ class PatchText:
     text: str
 
 
-@dataclass(slots=True, frozen=True)
-class PatchChildren:
-    children: list[Any]
+Patch = Union[PatchProps, PatchInsertChild, PatchRemoveChild, PatchText]
 
 
-Patch = PatchReplace | PatchProps | PatchText | PatchChildren
-
-
-class Node(Protocol):
+class Target(Protocol):
     def unwrap(self) -> Any:
         ...
 
@@ -38,14 +38,11 @@ class Node(Protocol):
         ...
 
 
-def with_node(cls: Any):
-    class WithNode(cls):
-        node: Node
+def with_target(cls: Any):
+    class WithTarget(cls):
+        target: Target
 
-    return WithNode
-
-
-T = TypeVar("T")
+    return WithTarget
 
 
 @dataclass(slots=True, frozen=True)
@@ -61,134 +58,305 @@ class Text:
 
 
 @dataclass(slots=True, frozen=True)
-class VirtualNodeElement(Element["VirtualNodeElement"]):
+class VDomElement(Element["VDomElement"]):
     pass
 
 
 @dataclass(slots=True, frozen=True)
-class VirtualNodeText(Text):
+class VDomText(Text):
     pass
 
 
-VirtualNode: TypeAlias = VirtualNodeElement | VirtualNodeText
+VDom: TypeAlias = Union[VDomElement, VDomText]
 
 
 @dataclass(slots=True, frozen=True)
-@with_node
-class MirrorNodeElement(Element["MirrorNodeElement"]):
-    def update(
-        self, props: dict[str, Any] | None = None, children: list[Any] | None = None
-    ) -> MirrorNodeElement:
-        patches = []
-        if props is None:
-            props = {}
-        if children is None:
-            children = []
+@with_target
+class NodeElement(Element["NodeElement"]):
+    pass
 
-        if props != self.props:
-            patches.append(PatchProps(props))
-        if children != self.children:
-            patches.append(PatchChildren(children=[c.node.unwrap() for c in children]))
 
-        if len(patches) == 0:
-            return self
-
-        for p in patches:
-            self.node.apply(p)
-        return MirrorNodeElement(
-            tag=self.tag,
-            props=props,
-            children=children,
-            node=self.node,
-        )
-
-    @staticmethod
-    def of(virtual_node: VirtualNodeElement, cls: type[Node]) -> MirrorNodeElement:
-        def _dispatch(virtual_node: VirtualNode, cls: type[Node]) -> MirrorNode:
-            match virtual_node:
-                case VirtualNodeElement():
-                    return MirrorNodeElement.of(virtual_node, cls)
-                case VirtualNodeText():
-                    return MirrorNodeText.of(virtual_node, cls)
-
-        new_children = [_dispatch(v_c, cls) for v_c in virtual_node.children]
-
-        node = cls()
-        node.apply(
-            PatchReplace(
-                tag=virtual_node.tag,
-                props=virtual_node.props,
-                children=[c.node.unwrap() for c in new_children],
-            )
-        )
-        return MirrorNodeElement(
-            tag=virtual_node.tag,
-            props=virtual_node.props,
-            children=new_children,
-            node=node,
-        )
+#    def patch(self, cls: type[Node], virtual_node: VDomElement) -> 'MirrorNode':
+#        new_children = []
+#        for m_c, v_c in zip_longest(self.children, virtual_node.children):
+#            res = patch2(cls, m_c, v_c)
+#            if res[0]:
+#                new_children.append(res[0])
+#                for p in res[1]:
+#                    self.node.apply(p)
+#        return NodeElement(
+#            tag=virtual_node.tag,
+#            props=virtual_node.props,
+#            children=new_children,
+#            node=self.node,
+#        )
+#
+#    def update(
+#        self,
+#        props: Optional[dict[str, Any]] = None,
+#        children: Optional[list[Any]] = None,
+#    ) -> 'NodeElement':
+#        patches = []
+#        if props is None:
+#            props = {}
+#        if children is None:
+#            children = []
+#
+#        if props != self.props:
+#            patches.append(PatchProps(props))
+#        if children != self.children:
+#            patches.append(PatchChildren(children=[c.node.unwrap() for c in children]))
+#
+#        if len(patches) == 0:
+#            return self
+#
+#        for p in patches:
+#            self.node.apply(p)
+#        return NodeElement(
+#            tag=self.tag,
+#            props=props,
+#            children=children,
+#            node=self.node,
+#        )
+#
+#    @staticmethod
+#    def of(virtual_node: VDomElement, cls: type[Node]) -> 'NodeElement':
+#        def _dispatch(virtual_node: VirtualNode, cls: type[Node]) -> MirrorNode:
+#            match virtual_node:
+#                case VDomElement():
+#                    return NodeElement.of(virtual_node, cls)
+#                case VirtualNodeText():
+#                    return MirrorNodeText.of(virtual_node, cls)
+#
+#        new_children = [_dispatch(v_c, cls) for v_c in virtual_node.children]
+#
+#        node = cls()
+#        node.apply(
+#            PatchReplace(
+#                tag=virtual_node.tag,
+#                props=virtual_node.props,
+#                children=[c.node.unwrap() for c in new_children],
+#            )
+#        )
+#        return NodeElement(
+#            tag=virtual_node.tag,
+#            props=virtual_node.props,
+#            children=new_children,
+#            node=node,
+#        )
 
 
 @dataclass(slots=True, frozen=True)
-@with_node
-class MirrorNodeText(Text):
-    def update(self, text: str) -> MirrorNodeText:
-        if self.text == text:
-            return self
-
-        self.node.apply(PatchText(text=text))
-        return MirrorNodeText(text=text, node=self.node)
-
-    @staticmethod
-    def of(virtual_node: VirtualNodeText, cls: type[Node]) -> MirrorNodeText:
-        node = cls()
-        node.apply(PatchText(text=virtual_node.text))
-        return MirrorNodeText(text=virtual_node.text, node=node)
+@with_target
+class NodeText(Text):
+    pass
 
 
-MirrorNode: TypeAlias = MirrorNodeElement | MirrorNodeText
+#    def update(self, text: str) -> 'NodeText':
+#        if self.text == text:
+#            return self
+#
+#        self.node.apply(PatchText(text=text))
+#        return NodeText(text=text, node=self.node)
+#
+#    @staticmethod
+#    def of(virtual_node: VirtualNodeText, cls: type[Node]) -> 'NodeText':
+#        node = cls()
+#        node.apply(PatchText(text=virtual_node.text))
+#        return NodeText(text=virtual_node.text, node=node)
 
 
-def patch(
-    cls: type[Node], mirror_node: MirrorNode | None, virtual_node: VirtualNode | None
-) -> MirrorNode | None:
-    match (mirror_node, virtual_node):
+Node: TypeAlias = Union[NodeElement, NodeText]
+
+# to support key
+
+
+@dataclass
+class PatchResult:
+    node: Optional[Node]
+    patches_to_parent: list[Patch]
+
+
+def _patch(
+    create_element: Callable[
+        [str, dict[str, Any], list[Any], Callable[[Any], None]], Target
+    ],
+    create_text: Callable[[str], Target],
+    dispatch: Callable[[Any], None],
+    node: Optional[Node],
+    vdom: Optional[VDom],
+) -> PatchResult:
+    match (node, vdom):
+        case (None, None):
+            return PatchResult(node=None, patches_to_parent=[])
         case (_, None):
-            return None
+            return PatchResult(
+                node=None,
+                patches_to_parent=[PatchRemoveChild(child=node.target.unwrap())],
+            )
+        case (None, VDomText(text)):
+            target = create_text(text)
+            return PatchResult(
+                node=NodeText(text=text, target=target),
+                patches_to_parent=[
+                    PatchInsertChild(child=target.unwrap(), reference=None)
+                ],
+            )
+        case (NodeElement(), VDomText(text)):
+            new_target = create_text(text)
+            return PatchResult(
+                node=NodeText(text=text, target=new_target),
+                patches_to_parent=[
+                    PatchInsertChild(child=new_target.unwrap(), reference=node.target),
+                    PatchRemoveChild(child=node.target.unwrap()),
+                ],
+            )
+        case (NodeText(node_text), VDomText(vdom_text)):
+            if node_text == vdom_text:
+                return PatchResult(node=node, patches_to_parent=[])
+
+            node.target.apply(PatchText(text=vdom_text))
+            return PatchResult(
+                node=NodeText(text=vdom_text, target=node.target), patches_to_parent=[]
+            )
+
+        case (None, VDomElement(tag, props, children)):
+            node_children = []
+            target = create_element(tag, props, [], dispatch)
+            for c in children:
+                ret = _patch(create_element, create_text, dispatch, None, c)
+                node_children.append(ret.node)
+                for p in ret.patches_to_parent:
+                    target.apply(p)
+
+            return PatchResult(
+                node=NodeElement(
+                    tag=tag, props=props, children=node_children, target=target
+                ),
+                patches_to_parent=[
+                    PatchInsertChild(child=target.unwrap(), reference=None)
+                ],
+            )
         case (
-            MirrorNodeText() as mirror_node,
-            VirtualNodeText() as virtual_node,
-        ):
-            return mirror_node.update(virtual_node.text)
-        case (_, VirtualNodeText() as virtual_node):
-            return MirrorNodeText.of(virtual_node, cls)
-        case (
-            MirrorNodeElement() as mirror_node,
-            VirtualNodeElement() as virtual_node,
-        ) if mirror_node.tag == virtual_node.tag:
+            NodeElement(node_tag, node_props, node_children, target),
+            VDomElement(tag, props, children),
+        ) if node_tag == tag:
+            if node_props != props:
+                del_keys = set(node_props.keys()) - set(props.keys())
+                add_props = {
+                    k: v
+                    for k, v in props.items()
+                    if k not in node_props or (k in node_props and node_props[k] != v)
+                }
+                target.apply(PatchProps(remove_keys=del_keys, add_props=add_props))
+
             new_children = []
-            for m_c, v_c in zip_longest(mirror_node.children, virtual_node.children):
-                if n := patch(cls, m_c, v_c):
-                    new_children.append(n)
-            return mirror_node.update(virtual_node.props, new_children)
-        case (_, VirtualNodeElement() as virtual_node):
-            return MirrorNodeElement.of(virtual_node, cls)
-        case (_, _):
-            raise ValueError("Invalid patch")
-    raise ValueError("e")
+            for n, vd in zip_longest(node_children, children):
+                ret = _patch(create_element, create_text, dispatch, n, vd)
+                if ret.node is not None:
+                    new_children.append(ret.node)
+                for p in ret.patches_to_parent:
+                    target.apply(p)
+            return PatchResult(
+                node=NodeElement(
+                    tag=tag, props=props, children=new_children, target=target
+                ),
+                patches_to_parent=[],
+            )
+        case (_, VDomElement(tag, props, children)):
+            node_children = []
+            new_target = create_element(tag, props, [], dispatch)
+            for c in children:
+                ret = _patch(create_element, create_text, dispatch, None, c)
+                node_children.append(ret.node)
+                for p in ret.patches_to_parent:
+                    new_target.apply(p)
+
+            return PatchResult(
+                node=NodeElement(
+                    tag=tag, props=props, children=node_children, target=new_target
+                ),
+                patches_to_parent=[
+                    PatchInsertChild(child=new_target.unwrap(), reference=node.target),
+                    PatchRemoveChild(child=node.target.unwrap()),
+                ],
+            )
 
 
-def element(
+def to_vdom(node: Node) -> VDom:
+    match node:
+        case (NodeElement(tag, props, children)):
+            return VDomElement(
+                tag=tag, props=props, children=[to_vdom(c) for c in children]
+            )
+        case (NodeText(text)):
+            return VDomText(text=text)
+    raise ValueError(f"Unknown node type: {node}")
+
+
+def el(
     tag: str,
-    props: dict[str, Any] | None = None,
-    children: list[VirtualNode] | None = None,
-) -> VirtualNodeElement:
+    props: Optional[dict[str, Any]] = None,
+    children: Optional[list[VDom]] = None,
+) -> VDomElement:
     if props is None:
         props = {}
     if children is None:
         children = []
-    return VirtualNodeElement(tag=tag, props=props, children=children)
+    return VDomElement(tag=tag, props=props, children=children)
 
 
-def text(text: str) -> VirtualNodeText:
-    return VirtualNodeText(text=text)
+def text(text: str) -> VDomText:
+    return VDomText(text=text)
+
+
+def make_patch(
+    create_element: Callable[
+        [str, dict[str, Any], list[Any], Callable[[Any], None]], Target
+    ],
+    create_text: Callable[[str], Target],
+    dispatch: Optional[Callable[[Any], None]] = None,
+) -> Callable[[Optional[Node], Optional[VDom]], PatchResult]:
+    return lambda node, vdom: _patch(
+        create_element=create_element,
+        create_text=create_text,
+        dispatch=dispatch,
+        node=node,
+        vdom=vdom,
+    )
+
+
+S = TypeVar("S")
+M = TypeVar("M")
+
+
+@dataclass(frozen=True)
+class UpdateResult(Generic[S]):
+    state: S
+    effects: list[Callable]
+
+
+def app(
+    create_element: Callable[
+        [str, dict[str, Any], list[Any], Callable[[Any], None]], Target
+    ],
+    create_text: Callable[[str], Target],
+    mount: Callable[[Target], None],
+    init: Callable[[], UpdateResult[S]],
+    view: Callable[[S], VDom],
+    update: Callable[[M, S], UpdateResult[S]],
+) -> None:
+    def dispatch(msg):
+        nonlocal state
+        nonlocal node
+        update_result = update(msg, state)
+        state = update_result.state
+        patch_result = patch(node, view(state))
+        node = patch_result.node
+
+    patch = make_patch(create_element, create_text, dispatch)
+
+    state = init()
+    ret = patch(None, view(state))
+    node = ret.node
+    mount(node.target)
--- tests/test_vdom.py
+++ tests/test_vdom.py
@@ -1,31 +1,31 @@
-from typing import Any
+from dataclasses import dataclass
+from typing import Any, Callable, Optional, Union
 
 import pytest
 
 from alfort.vdom import (
-    MirrorNode,
-    MirrorNodeElement,
-    MirrorNodeText,
-    Node,
     Patch,
-    PatchChildren,
+    PatchInsertChild,
     PatchProps,
-    PatchReplace,
+    PatchRemoveChild,
     PatchText,
-    VirtualNode,
-    VirtualNodeElement,
-    VirtualNodeText,
-    element,
-    patch,
+    Target,
+    UpdateResult,
+    VDom,
+    VDomElement,
+    VDomText,
+    app,
+    el,
+    make_patch,
     text,
+    to_vdom,
 )
 
 
-class MockNode(Node):
+class MockTarget(Target):
     _patches: list[Patch]
 
     def __init__(self) -> None:
-        super().__init__()
         self._patches = []
 
     def unwrap(self) -> Any:
@@ -35,141 +35,245 @@ class MockNode(Node):
         self._patches.append(patch)
 
 
-class VNodeNode(Node):
-    _node: VirtualNode | None
+class VDomTarget(Target):
+    _vdom: VDom
+    _dispatch: Optional[Callable[[Any], None]]
 
-    def __init__(self) -> None:
-        super().__init__()
-        self._node = None
+    def __init__(self, vdom: VDom, dispatch: Callable[[Any], None]) -> None:
+        self._vdom = vdom
+        self._dispatch = dispatch
 
     def unwrap(self) -> Any:
-        return self._node
+        return self._vdom
 
     def apply(self, patch: Patch) -> None:
         match patch:
-            case PatchReplace():
-                self._node = VirtualNodeElement(patch.tag, patch.props, patch.children)
-            case PatchChildren():
-                if not isinstance(self._node, VirtualNodeElement):
-                    raise ValueError("Only to apply children to elements")
+            case PatchInsertChild(child, reference):
+                ind = len(self._vdom.children)
+                if reference is not None:
+                    ind = self._vdom.children.index(reference)
+                self._vdom.children.insert(ind, child)
+            case PatchRemoveChild(child):
+                self._vdom.children.remove(child)
+            case PatchProps(remove_keys, add_props):
+                for k in remove_keys:
+                    del self._vdom.props[k]
+                for k, v in add_props.items():
+                    self._vdom.props[k] = v
+            case PatchText(text):
+                object.__setattr__(self._vdom, "text", text)
 
-                self._node = VirtualNodeElement(
-                    self._node.tag, self._node.props, patch.children
-                )
-            case PatchProps():
-                if not isinstance(self._node, VirtualNodeElement):
-                    raise ValueError("Only to apply props to an element node")
+    @staticmethod
+    def el(
+        tag: str,
+        props: dict,
+        children: list,
+        dispatch: Optional[Callable[[Any], None]] = None,
+    ) -> "VDomTarget":
+        return VDomTarget(
+            vdom=VDomElement(tag=tag, props=props, children=children), dispatch=dispatch
+        )
 
-                self._node = VirtualNodeElement(
-                    self._node.tag, patch.props, self._node.children
-                )
-            case PatchText():
-                self._node = VirtualNodeText(patch.text)
+    @staticmethod
+    def text(
+        text: str, dispatch: Optional[Callable[[Any], None]] = None
+    ) -> "VDomTarget":
+        return VDomTarget(vdom=VDomText(text=text), dispatch=dispatch)
 
 
-def test_construct_virtual_node() -> None:
-    node = element("div", props={"display": "flex"}, children=[text("abc")])
+def test_construct_vdom():
+    vdom = el(
+        "div", {"width": "100px"}, [text("hello"), el("span", {}, [text("world")])]
+    )
 
-    assert node.tag == "div"
-    assert node.props == {"display": "flex"}
-    assert len(node.children) == 1
-    assert node.children[0] == VirtualNodeText("abc")
+    assert vdom.tag == "div"
+    assert vdom.props == {"width": "100px"}
+    assert len(vdom.children) == 2
+    assert isinstance(vdom.children[0], VDomText)
+    assert vdom.children[0].text == "hello"
+    assert isinstance(vdom.children[1], VDomElement)
+    assert vdom.children[1].tag == "span"
+    assert vdom.children[1].props == {}
+    assert len(vdom.children[1].children) == 1
 
 
 @pytest.mark.parametrize(
-    "mirror_node_root, virtual_node_root, expected_patches",
+    "old_vdom, new_vdom, expected_patches, expected_root_patches",
     [
         (
             None,
-            element("div", {"display": "flex"}),
-            [PatchReplace("div", {"display": "flex"}, [])],
+            el("div", {"display": "flex"}),
+            [],
+            [PatchInsertChild],
         ),
         (
             None,
-            element("div", {"display": "flex"}, [element("br"), element("br")]),
-            [
-                PatchReplace(
-                    "div",
-                    {"display": "flex"},
-                    [[PatchReplace("br", {}, [])], [PatchReplace("br", {}, [])]],
-                )
-            ],
+            el("div", {"display": "flex"}, [el("br"), el("br")]),
+            [PatchInsertChild, PatchInsertChild],
+            [PatchInsertChild],
         ),
         (
-            MirrorNodeText("abc", node=MockNode()),
-            element("div", {"display": "flex"}),
-            [PatchReplace("div", {"display": "flex"}, [])],
-        ),
-        (
-            MirrorNodeElement(
+            el(
                 "div",
                 {"display": "absolute", "width": "100px"},
-                children=[],
-                node=MockNode(),
+                [el("br"), text("abc"), el("br"), el("br")],
             ),
-            element("div", {"display": "flex", "height": "100px"}),
-            [PatchProps(props={"display": "flex", "height": "100px"})],
+            el(
+                "div",
+                {"display": "flex", "height": "100px"},
+                [el("br"), text("hello"), text("world")],
+            ),
+            [
+                PatchProps,
+                PatchText,
+                PatchInsertChild,
+                PatchRemoveChild,
+                PatchRemoveChild,
+            ],
+            [],
         ),
         (
-            MirrorNodeElement("div", {"display": "flex"}, children=[], node=MockNode()),
-            element("div", {"display": "flex"}, children=[element("br")]),
-            [PatchChildren([[PatchReplace("br", {}, [])]])],
+            el(
+                "div",
+                {},
+                children=[],
+            ),
+            None,
+            [],
+            [PatchRemoveChild],
         ),
         (
             None,
             text("abc"),
-            [PatchText("abc")],
-        ),
-        (
-            MirrorNodeText("def", node=MockNode()),
-            text("abc"),
-            [PatchText("abc")],
-        ),
-        (
-            MirrorNodeElement("div", {"display": "flex"}, children=[], node=MockNode()),
-            element("div", {"display": "flex"}),
             [],
+            [PatchInsertChild],
         ),
+        (None, None, [], []),
     ],
 )
 def test_make_patch(
-    mirror_node_root: MirrorNode | None,
-    virtual_node_root: VirtualNode | None,
-    expected_patches: list[Patch],
+    old_vdom: VDom | None,
+    new_vdom: VDom | None,
+    expected_patches: list[type[Patch]],
+    expected_root_patches: list[type[Patch]],
 ) -> None:
-    root = patch(MockNode, mirror_node_root, virtual_node_root)
+    mock_target = MockTarget()
 
-    if root is None:
-        assert virtual_node_root is None
-    else:
-        assert root.node.unwrap() == expected_patches
+    def create_element(
+        tag: str, props: dict, children: list[Any], dispatch
+    ) -> MockTarget:
+        return mock_target
+
+    def create_text(text: str) -> MockTarget:
+        return mock_target
+
+    patch = make_patch(create_element, create_text)
+
+    ret = patch(None, old_vdom)
+    mock_target.unwrap().clear()
+    node = ret.node
+
+    ret = patch(node, new_vdom)
+    assert [type(p) for p in ret.patches_to_parent] == expected_root_patches
+    assert [type(p) for p in mock_target.unwrap()] == expected_patches
+
+    patched_vdom = ret.node
+    if patched_vdom is not None:
+        patched_vdom = to_vdom(patched_vdom)
+    assert patched_vdom == new_vdom
 
 
-@pytest.mark.parametrize(
-    "old_node_root, new_node_root",
-    [
-        (None, element("div", {"display": "flex"})),
-        (element("div", {"display": "flex"}), element("div", {"display": "flex"})),
-        (
-            element("div", {"display": "flex"}),
-            element("div", {"display": "flex"}, [element("br"), text("abc")]),
-        ),
-        (
-            element("br"),
-            element("div", {"display": "flex"}, [element("br"), text("abc")]),
-        ),
-        (element("div", children=[text("abc")]), text("abc")),
-    ],
-)
-def test_patch_nodes(
-    old_node_root: VirtualNode | None, new_node_root: VirtualNode | None
-) -> None:
-    mirror_node_root = patch(VNodeNode, None, old_node_root)
-    patched_node_root = patch(
-        VNodeNode, mirror_node=mirror_node_root, virtual_node=new_node_root
+def test_app():
+    def create_element(
+        tag: str, props: dict, children: list[Any], dispatch
+    ) -> MockTarget:
+        return VDomTarget.el(tag, props, children)
+
+    def create_text(text: str) -> MockTarget:
+        return VDomTarget.text(text)
+
+    def view(state: dict[str, int]) -> VDom:
+        return el(
+            "div",
+            {},
+            [
+                el("span", {}, [text("count: ")]),
+                el("span", {}, [text(str(state["count"]))]),
+            ],
+        )
+
+    def init():
+        return {"count": 0}
+
+    root = None
+
+    def mount(target: Target) -> None:
+        nonlocal root
+        root = target.unwrap()
+
+    app(
+        create_element,
+        create_text,
+        mount=mount,
+        init=init,
+        view=view,
+        update=lambda state, _: state,
     )
 
-    if patched_node_root is None:
-        assert patched_node_root is new_node_root is None
-    else:
-        assert patched_node_root.node.unwrap() == new_node_root
+    assert root == view(init())
+
+
+def test_event():
+    def create_element(
+        tag: str, props: dict, children: list[Any], dispatch
+    ) -> MockTarget:
+        return VDomTarget.el(tag, props, children, dispatch)
+
+    def create_text(text: str) -> MockTarget:
+        return VDomTarget.text(text)
+
+    def view(state: dict[str, int]) -> VDom:
+        return el(
+            "div",
+            {},
+            [
+                text(str(state["count"])),
+            ],
+        )
+
+    def init():
+        return {"count": 0}
+
+    @dataclass(frozen=True)
+    class CountUp:
+        pass
+
+    @dataclass(frozen=True)
+    class CountDown:
+        pass
+
+    Msg = Union[CountUp, CountDown]
+
+    def update(msg: Msg, state: dict[str, int]) -> UpdateResult[dict[str, int]]:
+        match msg:
+            case CountUp():
+                return UpdateResult(state={"count": state["count"] + 1}, effects=[])
+            case CountDown():
+                return UpdateResult(state={"count": state["count"] - 1}, effects=[])
+
+    dispatch = None
+    root = None
+
+    def mount(target: Target) -> None:
+        nonlocal root
+        nonlocal dispatch
+        root = target.unwrap()
+        dispatch = target._dispatch
+
+    app(create_element, create_text, mount=mount, init=init, view=view, update=update)
+
+    assert root == view({"count": 0})
+    dispatch(CountUp())
+    assert root == view({"count": 1})
+    dispatch(CountDown())
+    assert root == view({"count": 0})
